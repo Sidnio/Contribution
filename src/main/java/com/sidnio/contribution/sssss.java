19738e1a2d9dcf98d106cd5392f3bef176cdc2de
Sidnio <3196797522@qq.com>
2024-05-30 09:07:57 +0800

    实现中间显示图标

--- src/main/java/com/sidnio/contribution/sssss.java
+++ src/main/java/com/sidnio/contribution/sssss.java
@@ -4,26 +4,11 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.ui.Messages;
 import icons.ContributeIcons;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
-
-public class MyPluginAction extends AnAction {
-
-
-    public MyPluginAction() {
-       this(ContributeIcons.LogosActiveCampaign);
-    }
-
-    public MyPluginAction(@Nullable Icon icon) {
-        super(icon);
-
-    }
+public class sssss extends AnAction {
 
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-
+    public void actionPerformed(AnActionEvent e) {
         System.out.println("dddddddddddddddddddddddddddddddddd");
         Messages.showMessageDialog(e.getProject(), "Hello from My Plugin!", "Information", ContributeIcons.contribute);
     }
--- src/main/java/icons/ContributeIcons.java
+++ src/main/java/icons/ContributeIcons.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 
 public class ContributeIcons {
    public static final Icon contribute = load("/icons/contribute.svg");
+   public static final Icon LogosActiveCampaign = load("/icons/LogosActiveCampaign.svg");
 
     public static Icon load(String path) {
         return IconLoader.getIcon(path, ContributeIcons.class);
